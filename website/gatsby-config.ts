fbaec10cd8f545f5e0d4626707adbb264400f4f8
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-05-18 20:51:19 +0200

    website - set up gatsby project

---
+++ website/gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `My Gatsby Site`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
+
+export default config;
---
+++ website/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const IndexPage = () => {
+    return (
+        <main>
+            <h1 className='text-3xl text-teal-400 underline'>
+                Congratulations
+            </h1>
+        </main>
+    );
+};
+
+export default IndexPage;
