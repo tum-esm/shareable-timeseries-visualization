c779f2bbfdbc1a8a20d56a1bac83e92a5e601e73
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-05-19 02:31:33 +0200

    client - include mechanism when schema has changed

--- client/stv_client.py
+++ client/stv_client.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import sys
 import mysql.connector
 import os
 import json
@@ -22,7 +23,7 @@ class STVClient:
         self.database_name = database_name
         self.table_name = table_name
 
-        self.validate_schema_format()
+        self.__validate_schema_format()
 
         self.connection: mysql.connector.MySQLConnection = mysql.connector.connect(
             host="esm-mysql-public-do-user-7320955-0.b.db.ondigitalocean.com",
@@ -31,11 +32,16 @@ class STVClient:
             password=SQL_PASSWORD,
             database=database_name,
         )
-        if not self.table_exists():
-            self.create_table()
-        # TODO: drop and recreate table on schema change
+        if not self.__table_exists():
+            self.__create_table()
+        elif not self.__table_schemas_match():
+            self.__drop_table()
+            self.__create_table()
 
-    def validate_schema_format(self):
+    def __validate_schema_format(self):
+        """
+        Checks, whether the given schema is valid.
+        """
         reserved_keys = ["id", "date", "hour"]
         allowed_types = ["string", "float"]
         try:
@@ -49,7 +55,11 @@ class STVClient:
         except AssertionError as e:
             raise Exception(f"Invalid schema: {e}")
 
-    def validate_data_format(self, data: dict):
+    def __validate_data_format(self, data: dict):
+        """
+        Checks, whether the data is in the correct format with respect to the
+        given schema.
+        """
         try:
             assert isinstance(data, dict), "pass data in dict format"
             assert sorted(data.keys()) == sorted(
@@ -67,7 +77,19 @@ class STVClient:
         except AssertionError as e:
             raise Exception(f"Invalid data format: {e}")
 
-    def create_table(self):
+    def __drop_table(self):
+        if input("drop the existing table? (y) ").startswith("y"):
+            cursor = self.connection.cursor()
+            cursor.execute(f"DROP TABLE {self.table_name}", ())
+            self.connection.commit()
+            print(f"table {self.table_name} dropped")
+        else:
+            raise Exception("schema has changed, table has to be dropped")
+
+    def __create_table(self):
+        """
+        Creates the required table based on the give schema.
+        """
         cursor = self.connection.cursor()
         sql_types = {"string": "VARCHAR(32)", "float": "FLOAT", "int": "INT"}
         columns = [f"{key} {sql_types[value]}" for key, value in self.schema.items()]
@@ -83,13 +105,39 @@ class STVClient:
         self.connection.commit()
         print(f"table {self.table_name} created: {sql_statement}")
 
-    def table_exists(self):
+    def __table_exists(self):
+        """
+        Checks, whether the respective table exists.
+        """
         cursor = self.connection.cursor()
         cursor.execute("SHOW TABLES", ())
         return self.table_name in [r[0] for r in cursor.fetchall()]
 
+    def __table_schemas_match(self):
+        """
+        Checks, whether the respective table has the correct columns.
+        """
+        try:
+            cursor = self.connection.cursor()
+            cursor.execute(f"DESCRIBE {self.database_name}.{self.table_name}")
+            columns = cursor.fetchall()
+            assert columns[0] == ("ID", b"int", "NO", "PRI", None, "auto_increment")
+            assert columns[1][:3] == ("date", b"int", "NO")
+            assert columns[2][:3] == ("hour", b"float", "NO")
+            assert len(columns) == 3 + len(self.schema.items())
+            _i = 3
+            for key, datatype in self.schema.items():
+                assert columns[_i][0] == key
+                assert (
+                    columns[_i][1].decode().replace("varchar(32)", "string") == datatype
+                )
+                _i += 1
+            return True
+        except AssertionError:
+            return False
+
     def insert_data(self, data: dict):
-        self.validate_data_format(data)
+        self.__validate_data_format(data)
         now = datetime.now()
         date = now.strftime("%Y%m%d")
         hour = round(
