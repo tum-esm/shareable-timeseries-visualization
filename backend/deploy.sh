6a3d5594d9bcbc343b0e7ae06768ad6f9f8424eb
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-05-21 23:56:33 +0200

    backend - Finish new backend implementatin

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,10 +1,7 @@
-.venv/bin/pip freeze > requirements.txt
 docker build --platform linux/amd64 -t gcr.io/tum-esm/stv-backend .
 docker push gcr.io/tum-esm/stv-backend:latest
-rm requirements.txt
 
 COMMIT_SHA="$(git rev-parse --short --verify HEAD)"
-gcloud run deploy frontend \
+gcloud run deploy stv-backend \
     --image=gcr.io/tum-esm/stv-backend:latest \
-    --platform managed --no-traffic \
-    --tag "commit-$COMMIT_SHA"
+    --platform managed --tag "commit-$COMMIT_SHA"
--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,17 @@
 import mysql.connector
 from datetime import datetime, timedelta
 
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+from starlette.routing import Route
+
 connection: mysql.connector.MySQLConnection = mysql.connector.connect(
     host="esm-mysql-public-do-user-7320955-0.b.db.ondigitalocean.com",
     port=25060,
     user="public",
     password="AVNS_dCglpmL1fML93ap",
 )
+
 
 def string_is_clean(string: str):
     for character in string:
@@ -21,6 +26,7 @@ def string_is_clean(string: str):
         return False
     return True
 
+
 def enforce_date_on_datapoint(date: int, datapoint: tuple):
     if datapoint[1] != date:
         datapoint[1] = date
@@ -28,7 +34,7 @@ def enforce_date_on_datapoint(date: int, datapoint: tuple):
     return datapoint
 
 
-def get_schema():
+def get_schema(request):
     cursor = connection.cursor()
     cursor.execute(
         "SELECT table_schema, table_name, column_name, data_type "
@@ -49,9 +55,9 @@ def get_schema():
         if table_name not in schema[database_name].keys():
             schema[database_name][table_name] = {}
         schema[database_name][table_name][column_name] = column_type
-    
+
     clean_schema = {}
-    
+
     for database_name in schema.keys():
         for table_name in schema[database_name].keys():
             try:
@@ -62,73 +68,106 @@ def get_schema():
                 assert schema[database_name][table_name]["hour"] == "float"
                 assert schema[database_name][table_name]["sensor"] == "varchar"
                 data_column_names = list(
-                    filter(lambda c: c not in ["ID", "date", "hour", "sensor"], column_names)
+                    filter(
+                        lambda c: c not in ["ID", "date", "hour", "sensor"],
+                        column_names,
+                    )
                 )
                 assert all(
-                    [schema[database_name][table_name][c] == "float" for c in data_column_names]
+                    [
+                        schema[database_name][table_name][c] == "float"
+                        for c in data_column_names
+                    ]
                 )
                 if database_name not in clean_schema.keys():
                     clean_schema[database_name] = {}
-                clean_schema[database_name][table_name] = data_column_names                
+                clean_schema[database_name][table_name] = data_column_names
             except (AssertionError, KeyError) as e:
                 pass
-    return clean_schema
-            
+    return JSONResponse(clean_schema)
 
 
-def get_data(database_name: str, table_name: str):
-    assert string_is_clean(database_name)
-    assert string_is_clean(table_name)
-    cursor = connection.cursor()
-    cursor.execute(
-        f"SELECT date, hour FROM {database_name}.{table_name} "+
-        "ORDER BY date DESC, hour DESC LIMIT 1",
-        (),
-    )
-    result_rows = cursor.fetchall()
+def get_data(request):
+    try:
+        database_name = request.query_params["database"]
+        table_name = request.query_params["table"]
+        assert string_is_clean(database_name)
+        assert string_is_clean(table_name)
+    except KeyError:
+        return JSONResponse(
+            {"message": "missing or invalid query parameters"}, status_code=400
+        )
+
+    try:
+        cursor = connection.cursor()
+        cursor.execute(
+            f"SELECT date, hour FROM {database_name}.{table_name} "
+            + "ORDER BY date DESC, hour DESC LIMIT 1",
+            (),
+        )
+        result_rows = cursor.fetchall()
+    except mysql.connector.errors.ProgrammingError:
+        return JSONResponse({"message": "invalid database/table"}, status_code=400)
     if len(result_rows) == 0:
-        return []
-    
+        return JSONResponse([])
+
     # Starting with the newest measurement, return the newest 24 hours of data
     newest_date, newest_hour = result_rows[0]
     oldest_date = datetime.strftime(
-        datetime.strptime(str(newest_date), "%Y%m%d") - timedelta(days=1),
-        "%Y%m%d"
+        datetime.strptime(str(newest_date), "%Y%m%d") - timedelta(days=1), "%Y%m%d"
     )
     cursor.execute(
-        f"SELECT * FROM {database_name}.{table_name} WHERE (" +
-        f"  (date = {newest_date}) OR " +
-        f"  ((date = {oldest_date}) AND (hour >= {newest_hour}))" +
-        ") ORDER BY date DESC, hour DESC",
-        ()
-    ) 
-    return list(map(
-        lambda d: enforce_date_on_datapoint(newest_date, d),
-        cursor.fetchall()
-    ))
-    
-
-
-def get_meta_data(database_name: str, table_name: str):
-    assert string_is_clean(database_name)
-    assert string_is_clean(table_name)
-    cursor = connection.cursor()
-    cursor.execute(
-        "SELECT column_name, unit, description "
-        + f"FROM {database_name}.column_meta_data WHERE "
-        + f"table_name='{table_name}'",
+        f"SELECT * FROM {database_name}.{table_name} WHERE ("
+        + f"  (date = {newest_date}) OR "
+        + f"  ((date = {oldest_date}) AND (hour >= {newest_hour}))"
+        + ") ORDER BY date DESC, hour DESC",
         (),
     )
-    result_rows = cursor.fetchall()
+    return JSONResponse(
+        list(
+            map(lambda d: enforce_date_on_datapoint(newest_date, d), cursor.fetchall())
+        )
+    )
+
+
+def get_meta_data(request):
+    try:
+        database_name = request.query_params["database"]
+        table_name = request.query_params["table"]
+        assert string_is_clean(database_name)
+        assert string_is_clean(table_name)
+    except KeyError:
+        return JSONResponse(
+            {"message": "missing or invalid query parameters"}, status_code=400
+        )
+
+    assert string_is_clean(database_name)
+    assert string_is_clean(table_name)
+    try:
+        cursor = connection.cursor()
+        cursor.execute(
+            "SELECT column_name, unit, description "
+            + f"FROM {database_name}.column_meta_data WHERE "
+            + f"table_name='{table_name}'",
+            (),
+        )
+        result_rows = cursor.fetchall()
+    except mysql.connector.errors.ProgrammingError:
+        return JSONResponse({"message": "invalid database/table"}, status_code=400)
     meta_data = {}
     for column_name, unit, description in result_rows:
         if unit == "null":
             unit = None
         if description == "null":
             description = None
-        meta_data[column_name] = {
-            "unit": unit,
-            "description": description
-        }
-    return meta_data
+        meta_data[column_name] = {"unit": unit, "description": description}
+    return JSONResponse(meta_data)
 
+
+app = Starlette(
+    routes=[
+        Route("/schema", get_schema),
+        Route("/data", get_data),
+        Route("/meta-data", get_meta_data),
+    ]
+)
